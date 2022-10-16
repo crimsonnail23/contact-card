c8833f0e54cc66bec8500dbbe6313e307b4327d6
night <yseddiq@gmail.com>
2022-10-15 22:00:23 -0400

    updating database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,3 +14,20 @@ export const initdb = async()=>{
         }
     })
 }
+
+//export a function we will use to GET to the database.
+export const getDb = async ()=>{
+    //create a connected to the database and the version we want.
+    const contactDb = await openDB('contact_db',1);
+
+    //create a new transaction and specify the store and data priviledges.
+    const tx=contactDb.transaction('contacts', 'readonly');
+
+    //open up desired object store.
+    const store = tx.objectStore('contacts')
+
+    //use the getAll(); method to get all data in database.
+    const request = await store.getAll();
+
+    //get confimation
+}
