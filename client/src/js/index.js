33be6ce0089b4a4ddd16cd8a51c5433d8319acb7
night <yseddiq@gmail.com>
2022-10-15 20:09:33 -0400

    created and connected an indexedDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -12,8 +12,11 @@ import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+import{ initdb } from './database';
+
 // Add images on load
 window.addEventListener('load', function () {
+  initdb();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
